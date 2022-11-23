b2ac68a2c690a2bc9d6f7dcca1c9701c89caaefe
Dilnura [FE16] <102959751+Dilnura-d@users.noreply.github.com>
2022-11-23 14:43:13 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let obj = {
+    name:"Dilnura",
+    surname:"Shaymardanova",
+    age:"14",
+    study:"15",
+    hobbies:"skateboarding, rollerblading,programming and drawing,",
+    futurePlans:"enter to Parsons or FIT university in USA",
+}
+console.log("My name is",(obj.name),",and surname is",(obj.surname),".I'm",(obj.age),"years old","Now i'm studying at",(obj.study)+"nth school.","I'm interested at",(obj.hobbies),"And i hope in future i will",(obj.futurePlans))
+
+
+let pr = prompt('Son kiriting') 
+let pr2 = prompt("son kiriting")
+console.log(pr % pr2 )
